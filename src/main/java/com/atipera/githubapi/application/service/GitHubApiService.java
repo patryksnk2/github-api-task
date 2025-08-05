1e5f391a2572f1b7e4c93a82c484bbcc87cd02e8
patryksnk2 <patrykczerski83@gmail.com>
2025-08-05 21:23:14 +0200

    feat: add method to get branches from repository url

--- src/main/java/com/atipera/githubapi/application/service/GitHubApiService.java
+++ src/main/java/com/atipera/githubapi/application/service/GitHubApiService.java
@@ -1,6 +1,7 @@
 package com.atipera.githubapi.application.service;
 
 import com.atipera.githubapi.adapter.github.client.GitHubClient;
+import com.atipera.githubapi.adapter.github.dto.BranchDTO;
 import com.atipera.githubapi.adapter.github.dto.GitHubRepositoryDTO;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -15,5 +16,8 @@ public class GitHubApiService {
 
     public List<GitHubRepositoryDTO> getGitHubRepositories(@NonNull String username) {
         return client.getGitHubRepositories(username).stream().filter(repo -> !repo.fork()).toList();
+    }
+    public List<BranchDTO> getBranchesForRepo(@NonNull String repoUrl){
+        return client.getBranchesForRepo(repoUrl);
     }
 }

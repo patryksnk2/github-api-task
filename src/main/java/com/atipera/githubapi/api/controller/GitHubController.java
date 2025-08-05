79f411814489608057e038bdf010c93052eaffce
patryksnk2 <patrykczerski83@gmail.com>
2025-08-05 20:21:30 +0200

    feat: create GitHub controller and add method to get repositories by username

--- src/main/java/com/atipera/githubapi/api/controller/GitHubController.java
+++ src/main/java/com/atipera/githubapi/api/controller/GitHubController.java
@@ -1,4 +1,26 @@
-package com.atipera.githubapi.api.controller;
+package com.atipera.githubapi.application.controller;
 
+import com.atipera.githubapi.application.service.GitHubApiService;
+import com.atipera.githubapi.adapter.github.dto.GitHubRepositoryDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api.github")
 public class GitHubController {
+    private final GitHubApiService gitHubApiService;
+
+    @GetMapping("/{username}/repos")
+    public ResponseEntity<List<GitHubRepositoryDTO>> getRepositories( @Validated @PathVariable String username) {
+        List<GitHubRepositoryDTO> repositories = gitHubApiService.getGitHubRepositories(username);
+        return ResponseEntity.ok(repositories);
+    }
 }

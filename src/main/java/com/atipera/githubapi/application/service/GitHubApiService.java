b418cfe96d396d978d4b5a56cb991310f5b6b85e
patryksnk2 <patrykczerski83@gmail.com>
2025-08-05 20:10:06 +0200

    feat: implement service layer for GitHub repositories

--- src/main/java/com/atipera/githubapi/application/service/GitHubApiService.java
+++ src/main/java/com/atipera/githubapi/application/service/GitHubApiService.java
@@ -1,4 +1,19 @@
 package com.atipera.githubapi.application.service;
 
+import com.atipera.githubapi.adapter.github.client.GitHubClient;
+import com.atipera.githubapi.adapter.github.dto.GitHubRepositoryDTO;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class GitHubApiService {
+    private final GitHubClient client;
+
+    public List<GitHubRepositoryDTO> getGitHubRepositories(@NonNull String username) {
+        return client.getGitHubRepositories(username).stream().filter(repo -> !repo.fork()).toList();
+    }
 }
